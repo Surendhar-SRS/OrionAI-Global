78b6210bb0f754c07521c7300f3094cef3eab7a3
Surendhar S <surendharsurendhar436@gmail.com>
2025-08-16 19:46:24 +0530

    Created using Colab

---
+++ src/core/main.py
@@ -0,0 +1,30 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "gpuType": "V28",
+      "authorship_tag": "ABX9TyOVz0fQRCxRtdD8VIzjcwxf"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    },
+    "accelerator": "TPU"
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "tpht9riwMWWx"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
